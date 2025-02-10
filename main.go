507327ff7c0b3ae08127cfd19462703a5cacfa11
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2025-02-10 17:15:52 -0300

    feat: cpu profiling

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"log"
+	"math"
 	"net/http"
 	_ "net/http/pprof"
 	"time"
@@ -12,6 +13,7 @@ var globalStore [][]byte
 
 func main() {
 	go leakMemory()
+	go startCPUHeavyRoutine()
 
 	log.Println("Server running on :6060")
 	http.ListenAndServe(":6060", nil)
@@ -23,5 +25,19 @@ func leakMemory() {
 		globalStore = append(globalStore, data)
 
 		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func startCPUHeavyRoutine() {
+	for {
+		doHeavyComputation()
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func doHeavyComputation() {
+	sum := 0.0
+	for i := 0; i < 1000000; i++ {
+		sum += math.Sin(float64(i))
 	}
 }
